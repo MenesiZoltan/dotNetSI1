05839518067f0b62e368cb60206263b855079967
MenesiZoltan <colos.menesi@gmail.com>
2019-04-25 10:58:59 +0200

    Created Person Class

---
+++ CreateClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person("Géza", DateTime.Now, Gender.Male);
+            Console.WriteLine(person1.ToString());
+            Console.Write("IS THIS PRINTING AT ALL?");
+            Console.ReadLine();
+        }
+
+        class Person
+        {
+
+            private String name;
+            private DateTime birthDate;
+            private Gender gender;
+
+            public Person(String name, DateTime birthDate, Gender gender)
+            {
+
+                this.name = name;
+                this.birthDate = birthDate;
+                this.gender = gender;
+
+            }
+
+            override
+            public string ToString()
+            {
+                return name + " " + gender + " " + birthDate;
+            }
+
+        }
+
+        enum Gender
+        {
+
+            Male,
+            Female
+
+        }
+    }
+}
