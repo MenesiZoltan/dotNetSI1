ff6f4dd4f327da34da810a1f6fd7a0e243c5e44a
MenesiZoltan <colos.menesi@gmail.com>
2019-04-25 13:01:08 +0200

    Created Employee and Room class

--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -9,16 +9,26 @@ namespace CreateClass
 
             Person person1 = new Person("Géza", DateTime.Now, Gender.Male);
             Console.WriteLine(person1.ToString());
-            Console.Write("IS THIS PRINTING AT ALL?");
+            Console.WriteLine("\n");
+
+            Employee employee1 = new Employee("Zoli", DateTime.Now, Gender.Male, 10000, Profession.Necromancer);
+            Console.WriteLine(employee1.ToString());
+
+            Room room1 = new Room(employee1, 1);
+            Console.WriteLine("\n");
+            Console.WriteLine(room1.employee.ToString());
+            Console.WriteLine(room1.GetRoomNumber());
+
             Console.ReadLine();
+
         }
 
         class Person
         {
 
-            private String name;
-            private DateTime birthDate;
-            private Gender gender;
+            protected String name;
+            protected DateTime birthDate;
+            protected Gender gender;
 
             public Person(String name, DateTime birthDate, Gender gender)
             {
@@ -34,6 +44,54 @@ namespace CreateClass
             {
                 return name + " " + gender + " " + birthDate;
             }
+
+        }
+
+        class Employee: Person {
+
+            private int salary;
+            private Profession profession;
+
+            public Employee(string name, DateTime birthDate, Gender gender, int salary, Profession profession) : base(name, birthDate, gender)
+            {
+                this.profession = profession;
+                this.salary = salary;
+            }
+
+
+            override
+            public String ToString()
+            { return name + " " + gender + " " + birthDate + " " + profession + " " + salary; }
+
+        }
+
+        class Room {
+
+            public Employee employee;
+            private int roomNumber;
+
+            public Room(Employee employee, int roomNumber) {
+
+                this.employee = employee;
+                this.roomNumber = roomNumber;
+            }
+
+            public int GetRoomNumber() {
+                return roomNumber;
+            }
+
+        }
+
+        enum Profession {
+
+            Doctor,
+            Magician,
+            Necromancer,
+            Policeman,
+            Businessman,
+            HR,
+            Sales,
+            CEO
 
         }
 
